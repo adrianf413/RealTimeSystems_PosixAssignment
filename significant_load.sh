edfe292cd2ec1fa9135962c961b3c63c1a41d2e9
Adrian Forde <adrianforde4@gmail.com>
2020-03-16 23:28:11 +0000

    Changing bash for loop to different format

--- significant_load.sh
+++ significant_load.sh
@@ -4,7 +4,7 @@ RUNS=50
 mkdir csvout
 mkdir csvout/q2a
 mkdir csvout/q2a/${RUNS}runs
-for (( i=1 ; i<=$RUNS ; i++ ))
+for i in {0..50}
 do
 #make sure to change file name to whatever you named it
     ./timer_mem_use 5 1000 > csvout/q2a/${RUNS}runs/run${i}.csv &
