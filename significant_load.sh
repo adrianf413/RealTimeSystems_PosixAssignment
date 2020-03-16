5c32481b0fb492e3165c3ba3e6445c831c4d8222
Adrian Forde <adrianforde4@gmail.com>
2020-03-16 23:16:28 +0000

    Fixing syntax bash script

--- significant_load.sh
+++ significant_load.sh
@@ -5,6 +5,7 @@ mkdir csvout
 mkdir csvout/q2a
 mkdir csvout/q2a/${RUNS}runs
 for (( i=1; i<=$RUNS; i++ ))
-    do
-        ./q2a 5 1000 > csvout/q2a/${RUNS}runs/run${i}.csv &
-    done
+do
+#make sure to change file name to whatever you named it
+    ./timer_mem_use 5 1000 > csvout/q2a/${RUNS}runs/run${i}.csv &
+done
