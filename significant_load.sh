4c6650d9548e36fa259a40a87987e4118490dad3
Adrian Forde <adrianforde@debian.lab.local>
2020-03-19 17:00:19 +0000

    Adding CSV file results

--- significant_load.sh
+++ significant_load.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-RUNS=20
-mkdir csvout
-mkdir csvout/q2a
+RUNS=8
+#mkdir csvout
+#mkdir csvout/q2a
 mkdir csvout/q2a/${RUNS}runs
-for i in {0..20}
+for i in {1..8}
 do
-#make sure to change file name to whatever you named it
-    ./timer_mem_use 5 1000 > csvout/q2a/${RUNS}runs/run${i}.csv &
+    ./timer_mem_use 10000000 5 > csvout/q2a/${RUNS}runs/run${i}.csv &
 done
